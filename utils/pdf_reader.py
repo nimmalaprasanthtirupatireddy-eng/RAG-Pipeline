6971cfcc91bd6a4ebf01307ce9a982f3b5e9db7e
Nimmala Prasanth Tirupati Reddy <nimmalaprasanthtirupatireddy@gmail.com>
2026-02-06 16:05:09 +0530

    Changed from PyPDF2 to PyMuPDF4LLM

--- utils/pdf_reader.py
+++ utils/pdf_reader.py
@@ -32,28 +32,38 @@
 
 #     return text
 
-import os
-from PyPDF2 import PdfReader
+# import os
+# from PyPDF2 import PdfReader
+# from .logger import logger
+
+# def read_pdf_text(pdf_path, output_dir="extracted_text"):
+#     logger.info(f"Reading PDF: {pdf_path}")
+#     reader = PdfReader(pdf_path)
+#     text = ""
+
+#     for i, page in enumerate(reader.pages):
+#         page_text = page.extract_text() or ""
+#         text += page_text + "\n"
+#         logger.debug(f"Processed page {i+1}")
+
+#     logger.info(f"Finished reading PDF ({len(reader.pages)} pages)")
+
+#     os.makedirs(output_dir, exist_ok=True)
+#     output_path = os.path.join(output_dir, "combined.txt")
+
+#     with open(output_path, "a", encoding="utf-8") as f:
+#         f.write(f"\n\n===== FILE: {os.path.basename(pdf_path)} =====\n\n")
+#         f.write(text)
+
+#     logger.info(f"Appended extracted text to {output_path}")
+#     return text
+
+
 from .logger import logger
+import pymupdf4llm 
 
-def read_pdf_text(pdf_path, output_dir="extracted_text"):
+def read_pdf_text(pdf_path):
     logger.info(f"Reading PDF: {pdf_path}")
-    reader = PdfReader(pdf_path)
-    text = ""
-
-    for i, page in enumerate(reader.pages):
-        page_text = page.extract_text() or ""
-        text += page_text + "\n"
-        logger.debug(f"Processed page {i+1}")
-
-    logger.info(f"Finished reading PDF ({len(reader.pages)} pages)")
-
-    os.makedirs(output_dir, exist_ok=True)
-    output_path = os.path.join(output_dir, "combined.txt")
-
-    with open(output_path, "a", encoding="utf-8") as f:
-        f.write(f"\n\n===== FILE: {os.path.basename(pdf_path)} =====\n\n")
-        f.write(text)
-
-    logger.info(f"Appended extracted text to {output_path}")
+    text = pymupdf4llm.to_markdown(pdf_path)
+    logger.info(f"Finished reading PDF")
     return text
